f5feac440bff3e2444328db2981cb96a2074d084
Omar Segura <omarsegura28@gmail.com>
2025-09-20 16:01:52 -0500

    Todo en su lugar, lab22, lab23

---
+++ Laboratorio2/Laboratorio22/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Laboratorio22
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            //Asignando valor a variable estatica.
+            MyClass.Valor = 1;
+            Console.WriteLine(MyClass.Valor);
+        }
+    }
+        
+        public class MyClass
+        { //Declarando variable estatica
+            public static int Valor;
+        }
+   
+
+}
---
+++ Laboratorio2/Laboratorio23/Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int valor1 = 28;//declaramos valor1 con un valor de 28
+        int valor2 = valor1;//Asignamos el valor de valor1 a valor2
+        valor2 = 30;
+
+        //Imprimiendo en consola variables
+
+        Console.WriteLine(valor1);
+        Console.WriteLine(valor2);
+
+        Console.WriteLine(valor1);
+        Console.WriteLine(valor2);
+
+       
+       }
+}
