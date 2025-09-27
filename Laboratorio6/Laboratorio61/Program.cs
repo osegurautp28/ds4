7a426548394d899bc46638e145f830db153289fa
Omar Segura <omarsegura28@gmail.com>
2025-09-26 23:41:19 -0500

    Subiendo Lab6 completo

---
+++ Laboratorio6/Laboratorio61/Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int num;
+        Console.WriteLine("Digite el numero deseado");
+        try
+        {
+            num = Int16.Parse(Console.ReadLine());
+
+        }
+        catch (FormatException ex)
+        {
+            Console.WriteLine("No ha introducido un digito valido");
+            num = -1;
+        }
+        Console.WriteLine(num);
+    }
+}
---
+++ Laboratorio6/Laboratorio64/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    static void checkAge(int age)
+    {
+        if (age < 18)
+        {
+            throw new ArithmeticException("Acesso negado-no cumple con el criterio de edad");
+        }
+        else
+        {
+            Console.WriteLine("Acesso concedido");
+        }
+    }
+            private static void Main(string[] args)
+            {
+                checkAge(15);
+            }
+}
