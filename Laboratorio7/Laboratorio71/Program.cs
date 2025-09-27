bc58f7082449fcbb44b4fd4a8e9ec2a220995f65
Omar Segura <omarsegura28@gmail.com>
2025-09-27 17:20:58 -0500

    Subiendo Lab7 completo

---
+++ Laboratorio7/Laboratorio71/Program.cs
@@ -0,0 +1,15 @@
+﻿using Laboratorio71;
+
+internal class Program
+{
+    static void Main(string[] args)
+  {
+    
+        {
+            Banco banco1 = new Banco();
+            banco1.Operar();
+            banco1.DepositosTotales();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Laboratorio7/Laboratorio72/Program.cs
@@ -0,0 +1,13 @@
+﻿using Laboratorio72;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        
+            JuegoDeDados j = new JuegoDeDados();
+            j.Jugar();
+        
+
+    }
+}
